cb0c86e6e660488171c394ed5d444aa75785bf92
Tim Kozemzak <timkozemzak@gmail.com>
2018-06-21 18:47:54 -0700

    pt1 solutions

--- part1/server.js
+++ part1/server.js
@@ -2,9 +2,31 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.get('/hello', function(req, res) {
+  res.send("Hello!");
 });
+
+app.post('/create/:name', function(req, res){
+  let name = `{
+    "id":1,
+    "name":"${req.params.name}"
+  }`
+  res.send(name)
+})
+
+app.get('/', function(req, res){
+  res.sendFile('/Users/timothykozemzak/Desktop/Q2/expressIntroChallenges/part1/index.html')
+})
+
+app.get('/verify/:age', function(req, res){
+  let ageInput = req.params.age;
+  if(ageInput > 13){
+    res.send("All Good")
+  } else {
+    res.sendStatus(403)
+  }
+})
+
 
 app.use(function(req, res) {
   res.sendStatus(404);
--- part2/server.js
+++ part2/server.js
@@ -1,10 +1,41 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+var fs = require('fs')
+var names = 'e';
 
 app.get('/yourroute', function(req, res) {
   res.send("stuff");
 });
+
+
+//challenge1+stretch
+let id = 1;
+
+app.post("/create/:name/:age", function(req, res) {
+  id++;
+  let nameAge = `{
+    "id": ${id}
+    "name": ${req.params.name},
+    "age": ${req.params.age}
+  }`
+  res.send(nameAge)
+  names += `{
+    "name" : "${req.params.name}",
+    "age" : ${req.params.age}
+}`
+  names += fs.writeFileSync('./storage.json', names)
+})
+//challenge2
+app.get("/", function(req, res) {
+  res.sendFile("/Users/timothykozemzak/Desktop/Q2/expressIntroChallenges/part2/storage.json")
+})
+
+//challenge 3
+app.get("/:name", function(req, res) {
+console.log(names)
+})
+
 
 app.use(function(req, res) {
   res.sendStatus(404);
